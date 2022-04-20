cffa7ac061ea2750dc51f6b44d5cfd6fa222fc45
Bert Jansen <bjansen@microsoft.com>
2022-04-20 19:14:16 +0200

    Documentation site prep

--- docs/sql queries.sql
+++
@@ -1,9 +0,0 @@
-select * from Scans;
-select * from Properties;
-select * from SiteCollections;
-select * from SiteCollections where status = 4;
-select * from Webs;
-select distinct SiteUrl from Webs where status = 4;
-select * from TestDelays;
-select * from History;
-select * from Cache;
