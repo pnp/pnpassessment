c0b1ac5f9ae8c2a2f706b5dde440094c89484d6c
Bert Jansen <bjansen@microsoft.com>
2022-04-20 20:52:51 +0200

    Custom documentation style

--- docs/build.ps1
+++ docs/build.ps1
@@ -7,8 +7,8 @@ Set-StrictMode -Version 2.0
 docfx build ./main/docs/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
-#Remove-Item ./gh-pages/using-the-assessment-tool/* -Recurse -Force
-#Remove-Item ./gh-pages/sharepoint-syntex/* -Recurse -Force
-#Remove-Item ./gh-pages/contributing/* -Recurse -Force
-#Remove-Item ./gh-pages/images/* -Recurse -Force
+Remove-Item ./gh-pages/using-the-assessment-tool/* -Recurse -Force
+Remove-Item ./gh-pages/sharepoint-syntex/* -Recurse -Force
+Remove-Item ./gh-pages/contributing/* -Recurse -Force
+Remove-Item ./gh-pages/images/* -Recurse -Force
 copy-item -Force -Recurse ./main/docs/_site/* -Destination ./gh-pages
