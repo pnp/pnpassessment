c103929f14b719591aebd0ddd791b36429bdc3e5
Bert Jansen <jansenbe@users.noreply.github.com>
2022-04-20 20:30:19 +0200

    Update build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -4,11 +4,11 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
 #docfx metadata ./dev/docs/docfx.json --warningsAsErrors $args
-docfx build ./dev/docs/docfx.json --warningsAsErrors $args
+docfx build ./main/docs/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
 #Remove-Item ./gh-pages/using-the-assessment-tool/* -Recurse -Force
 #Remove-Item ./gh-pages/sharepoint-syntex/* -Recurse -Force
 #Remove-Item ./gh-pages/contributing/* -Recurse -Force
 #Remove-Item ./gh-pages/images/* -Recurse -Force
-copy-item -Force -Recurse ./dev/docs/_site/* -Destination ./gh-pages
+copy-item -Force -Recurse ./main/docs/_site/* -Destination ./gh-pages
