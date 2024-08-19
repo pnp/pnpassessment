33ec8c601a940a44c7593555bcc5c1cee733019a
bjansen <bjansen+odspmdb@microsoft.com>
2024-08-19 19:36:24 +0200

    updated signing script

--- build/signcurrentbuild.ps1
+++ build/signcurrentbuild.ps1
@@ -2,4 +2,4 @@
 
 # Sign the binaries
 Write-Host "Signing the binaries..."
-d:\github\SharePointPnP\CodeSigning\PnP\sign-pnpbinaries.ps1 -SignJson pnpassessment
+q:\github\SharePointPnP\CodeSigning\PnP\sign-pnpbinaries.ps1 -SignJson pnpassessment
