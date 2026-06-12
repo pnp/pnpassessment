b915d628e016d42c71f11798006ba6f6478f2d27
Zhenhua Zhu <zhenhzhu@126.com>
2026-06-12 14:57:37 +0800

    Mapping % + Unmapped list

---
+++ src/PnP.Scanning/PnP.Scanning.Core/Storage/DatabaseMigration/20260611110654_v1.12.1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PnP.Scanning.Core.Storage.DatabaseMigration
+{
+    /// <inheritdoc />
+    public partial class v1121 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "MappingPercentage",
+                table: "ClassicPages",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UnmappedWebParts",
+                table: "ClassicPages",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WebPartCount",
+                table: "ClassicPages",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MappingPercentage",
+                table: "ClassicPages");
+
+            migrationBuilder.DropColumn(
+                name: "UnmappedWebParts",
+                table: "ClassicPages");
+
+            migrationBuilder.DropColumn(
+                name: "WebPartCount",
+                table: "ClassicPages");
+        }
+    }
+}
